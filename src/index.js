87b8c57f15404ce9b0397df169e5b944244e369a
Jalen Adams <jalen@jalenkadams.me>
2020-07-19 09:59:52 -0400

    Add: tab bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import initHomepage from './homepage.js'
 
+function initTabBar() {
+    let tabBar = document.createElement("div");
+    tabBar.id = "tabBar";
+    tabBar.classList = ["tabs"];
+
+    let pageContent = document.querySelector("#content");
+    pageContent.appendChild(tabBar);
+}
+
+initTabBar();
 initHomepage();
